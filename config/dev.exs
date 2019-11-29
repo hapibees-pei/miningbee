45d264efe016550ccbae57e9f427882b3731de91
Daniel Tinoco <0urobor0s@users.noreply.github.com>
2019-11-29 16:00:20 +0000

    Change Postgres port (#8)

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :miningbee, Miningbee.Repo,
   hostname: "localhost",
   username: "postgres",
   password: "postgres",
+  port: 5433,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
