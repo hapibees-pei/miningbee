051607f932e484fc0c473c2087487c1497c080ab
Daniel Tinoco <0urobor0s@users.noreply.github.com>
2019-12-11 16:32:11 +0000

    Integration fixes (#20)

--- lib/miningbee/onboarding.ex
+++ lib/miningbee/onboarding.ex
@@ -3,7 +3,8 @@ defmodule Miningbee.Onboarding do
 
   def create_onboarding(ip, port, uuid) do
     # ip must be inside '' and not "" because of Erlang
-    with {:ok, _pid} <- TcpClient.start(to_charlist(ip), port, uuid) do
+    with {:ok, _pid} <-
+           TcpClient.start(to_charlist(ip), String.to_integer(port), uuid) do
       {:ok, "onboarding started"}
     end
   end
